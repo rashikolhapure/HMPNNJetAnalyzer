b3b6b02347eb780479fb42ee2d313ea60ca82e86
rajveer43 <rajveer.rathod1301@gmail.com>
2023-09-15 10:15:17 +0530

    Update exe_utils.py

--- exe_utils.py
+++ exe_utils.py
@@ -16,15 +16,19 @@ def get_from_indices(
     keys=None,
 ):
     """
-    Extract selected keys and corresponding indices from a dictionary of events.
+    Extract selected keys and corresponding indices from a dictionary of
+    events.
 
     Args:
         events (dict): A dictionary containing event data.
         indices (array-like): Indices to select from the event data.
-        keys (list of str, optional): A list of keys to extract from the events. If None, all keys are extracted.
+        keys (list of str, optional): A list of keys to extract from the
+            events. If None, all keys are extracted.
 
     Returns:
-        dict: A new dictionary containing the selected keys and their corresponding values.
+        dict: A new dictionary containing the selected keys and their
+        corresponding values for each index in `indices`. The returned value
+        is
     """
     if keys is None:
         keys = list(events.keys())
@@ -44,8 +48,10 @@ def get_delphes(run_names, **kwargs):
     Load and print Delphes event data from one or more run names.
 
     Args:
-        run_names (str or list of str): The name(s) of the Delphes run(s) to load.
-        **kwargs: Additional keyword arguments for DelphesNumpy initialization.
+        run_names (str or list of str): The name(s) of the Delphes run(s) to
+            load.
+        **kwargs: Additional keyword arguments for DelphesNumpy
+            initialization.
 
     Returns:
         None
@@ -65,8 +71,9 @@ def get_numpy_events(run_name, runs="first", **kwargs):
 
     Args:
         run_name (str): The name of the run to load.
-        runs (str, optional): Specify how to handle multiple runs if present ('first' to return the first run,
-            'merge' to merge all runs). Default is 'first'.
+        runs (str, optional): Specify how to handle multiple runs if present
+            ('first' to return the first run, 'merge' to merge all runs).
+            Default is 'first'.
         **kwargs: Additional keyword arguments for NumpyEvents initialization.
 
     Returns:
