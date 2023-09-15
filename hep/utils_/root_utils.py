f41bdf82811a27d7b1ab487dcd23444a4c61cb02
rajveer43 <rajveer.rathod1301@gmail.com>
2023-09-15 17:14:11 +0530

    Update root_utils.py

--- hep/utils_/root_utils.py
+++ hep/utils_/root_utils.py
@@ -30,20 +30,24 @@ def GetTLorentzVector(
     format="lhc",
     particle="visible",
 ):
-    """Get numpy.ndarray of elements TLorentzVector from <array> of elements either a PseudoJet or
-    numpy.ndarray in format: lhc with [pt,eta,phi] or [pt,eta,phi,mass] and lorentz with [px,py,pz,E]
-    """
-    """
-    Convert a numpy.ndarray of elements to TLorentzVector(s) with the specified format.
+    """Get numpy.ndarray of elements TLorentzVector from <array> of
+    elements either a PseudoJet or numpy.ndarray in format: lhc with
+    [pt,eta,phi] or [pt,eta,phi,mass] and lorentz with [px,py,pz,E]
+
+    Convert a numpy.ndarray of elements to TLorentzVector(s) with
+    the specified format.
 
     Parameters:
     ----------
     array : numpy.ndarray
-        The input array of elements to be converted to TLorentzVector(s).
+        The input array of elements to be converted to
+        TLorentzVector(s).
     format : str, optional
-        The desired output format. Choose from {"fatjet", "lhc", "lorentz", "image"}.
+        The desired output format. Choose from
+        {"fatjet", "lhc", "lorentz", "image"}.
     particle : str, optional
-        The type of particle for TLorentzVector construction (default is "visible").
+        The type of particle for TLorentzVector construction
+        (default is "visible").
 
     Returns:
     -------
@@ -59,7 +63,8 @@ def GetTLorentzVector(
 
     To convert a list of elements in "lhc" format to TLorentzVectors:
 
-    >>> elements = [np.array([30.0, 1.2, 0.5, 0.0]), np.array([40.0, -0.8, 2.1, 0.0])]
+    >>> elements = [np.array([30.0, 1.2, 0.5, 0.0]),
+    ...             np.array([40.0, -0.8, 2.1, 0.0])]
     >>> vectors = GetTLorentzVector(elements, format="lhc")
 
     To convert an array of elements in "image" format to TLorentzVectors:
@@ -81,7 +86,9 @@ def GetTLorentzVector(
             )
             count += 1
         return vec
-    # assert format in ("lhc","lorentz") and (array.shape[-2]==3 or array.shape[-1]==4) and len(array.shape)==2
+    # assert format in ("lhc","lorentz")
+    # and (array.shape[-2]==3 or array.shape[-1]==4)
+    # and len(array.shape)==2
     if len(array.shape) == 1:
         vec = TLorentzVector()
         if particle == "MET":
@@ -153,10 +160,11 @@ def GetNumpy(
     format="image",
     observable_first=True,
 ):
-    """convert TLorentzVector(s) to numpy.ndarray with format in {"image":[eta,phi,pt],"lhc":[pt,eta,phi,mass],"lorentz":[px,py,pz,E]
-    default observable_first=True gives shape of (len(format),constituents) otherwise the axes are swaped
-    """
-    """
+    """convert TLorentzVector(s) to numpy.ndarray with format in
+    {"image":[eta,phi,pt],"lhc":[pt,eta,phi,mass],"lorentz":[px,py,pz,E]
+    default observable_first=True gives shape of
+    (len(format),constituents) otherwise the axes are swaped.
+
     Convert TLorentzVector(s) to numpy.ndarray with specified format.
 
     Parameters:
@@ -164,9 +172,13 @@ def GetNumpy(
     vectors : TLorentzVector or list of TLorentzVectors
         The input TLorentzVector(s) to be converted.
     format : str, optional
-        The desired output format. Choose from {"image": [eta, phi, pt], "lhc": [pt, eta, phi, mass], "lorentz": [px, py, pz, E]}.
+        The desired output format. Choose from
+        {"image": [eta, phi, pt],
+         "lhc": [pt, eta, phi, mass],
+         "lorentz": [px, py, pz, E]}.
     observable_first : bool, optional
-        Whether the resulting array should have the observable as the first axis (default is True).
+        Whether the resulting array should have the observable
+        as the first axis (default is True).
 
     Returns:
     -------
@@ -182,7 +194,8 @@ def GetNumpy(
 
     To convert a list of TLorentzVectors to the "image" format:
 
-    >>> vectors = [TLorentzVector(1.0, 2.0, 3.0, 4.0), TLorentzVector(5.0, 6.0, 7.0, 8.0)]
+    >>> vectors = [TLorentzVector(1.0, 2.0, 3.0, 4.0),
+    ...            TLorentzVector(5.0, 6.0, 7.0, 8.0)]
     >>> array = GetNumpy(vectors, format="image")
     """
     if format == "image":
@@ -291,10 +304,10 @@ def Sort(
     attribute="pt",
     order="desc",
 ):
-    """sort a numpy.ndarray of TLorentzVectors with attribute in ("p","pt","px","py","pz","eta","phi","mass","E").
+    """sort a numpy.ndarray of TLorentzVectors with attribute in
+    ("p","pt","px","py","pz","eta","phi","mass","E").
     descending if <order> is "desc" ortherwise ascending
-    """
-    """
+
     Sort a numpy.ndarray of TLorentzVectors based on a specified attribute.
 
     Parameters:
@@ -302,9 +315,11 @@ def Sort(
     array : numpy.ndarray
         The array of TLorentzVectors to be sorted.
     attribute : str, optional
-        The attribute by which to sort the TLorentzVectors. Options are ("p", "pt", "px", "py", "pz", "eta", "phi", "mass", "E").
+        The attribute by which to sort the TLorentzVectors. Options are
+        ("p", "pt", "px", "py", "pz", "eta", "phi", "mass", "E").
     order : str, optional
-        The sorting order, "desc" for descending (default) or "asc" for ascending.
+        The sorting order, "desc" for descending (default) or "asc" for
+        ascending.
 
     Returns:
     -------
@@ -313,9 +328,11 @@ def Sort(
 
     Example:
     --------
-    To sort an array of TLorentzVectors by transverse momentum (pt) in descending order:
+    To sort an array of TLorentzVectors by transverse momentum (pt)
+    in descending order:
 
-    >>> vectors = np.array([TLorentzVector(1.0, 2.0, 3.0, 4.0), TLorentzVector(5.0, 6.0, 7.0, 8.0)])
+    >>> vectors = np.array([TLorentzVector(1.0, 2.0, 3.0, 4.0),
+    ...                     TLorentzVector(5.0, 6.0, 7.0, 8.0)])
     >>> sorted_vectors = Sort(vectors, attribute="pt", order="desc")
 
     To sort by energy (E) in ascending order:
@@ -354,22 +371,27 @@ def Print(
     name=None,
 ):
     """
-    Utility function to print information about a TLorentzVector or a list of TLorentzVectors.
+    Utility function to print information about a TLorentzVector or a list
+    of TLorentzVectors.
 
     Parameters:
     ----------
     vector : TLorentzVector or list of TLorentzVectors
         The TLorentzVector or list of TLorentzVectors to print.
     format : str, optional
-        The format in which to print the information, "lhc" (default) or "other."
+        The format in which to print the information,
+        "lhc" (default) or "other."
     name : str, optional
         A custom name for the printed content.
 
     Notes:
     -----
-    This function can print information about a single TLorentzVector or a list of TLorentzVectors.
-    If 'format' is set to "lhc," it prints Eta, Phi, Pt, Mass, and Momentum (P).
-    If 'format' is set to "other," it prints Px, Py, Pz, Energy (E), and Momentum (P).
+    This function can print information about a single TLorentzVector
+    or a list of TLorentzVectors.
+    If 'format' is set to "lhc," it prints Eta, Phi, Pt, Mass,
+    and Momentum (P).
+    If 'format' is set to "other," it prints Px, Py, Pz, Energy (E),
+    and Momentum (P).
 
     Example:
     --------
@@ -380,7 +402,8 @@ def Print(
 
     To print information about a list of TLorentzVectors:
 
-    >>> vectors = [TLorentzVector(1.0, 2.0, 3.0, 4.0), TLorentzVector(5.0, 6.0, 7.0, 8.0)]
+    >>> vectors = [TLorentzVector(1.0, 2.0, 3.0, 4.0),
+    ...            TLorentzVector(5.0, 6.0, 7.0, 8.0)]
     >>> Print(vectors, format="other")
     """
     if name is not None:
@@ -388,36 +411,46 @@ def Print(
     if isinstance(vector, TLorentzVector):
         if format == "lhc":
             print(
-                f"    Eta: {vector.Eta():20.16f}        Phi: {vector.Phi():20.16f}        Pt : {vector.Pt():20.16f}        Mass: {vector.M():20.16f}        P= {vector.P():20.16f}"
+                f"    Eta: {vector.Eta():20.16f}   Phi: {vector.Phi():20.16f}\
+                      Pt : {vector.Pt():20.16f}    Mass: {vector.M():20.16f}\
+                      P= {vector.P():20.16f}"
             )
         else:
             print(
-                f"    Px: {vector.Px():20.16f}        Py: {vector.Py():20.16f}        Pz : {vector.Pz():20.16f}        E: {vector.E():20.16f}        P={vector.P():20.16f}"
+                f"    Px: {vector.Px():20.16f}   Py: {vector.Py():20.16f}\
+                      Pz : {vector.Pz():20.16f}    E: {vector.E():20.16f}\
+                      P={vector.P():20.16f}"
             )
     else:
         print("Constituents of array: ")
         for item in vector:
             if format == "lhc":
                 print(
-                    f"    Eta: {item.Eta():20.16f}        Phi: {item.Phi():20.16f}        Pt : {item.Pt():20.16f}        Mass: {item.M():20.16f}        P= {item.P():20.16f}"
+                    f"    Eta: {item.Eta():20.16f}   Phi: {item.Phi():20.16f}\
+                          Pt : {item.Pt():20.16f}    Mass: {item.M():20.16f}\
+                          P= {item.P():20.16f}"
                 )
             else:
                 print(
-                    f"    Px : {item.Px():20.16f}        Py: {item .Py():20.16f}        Pz: {item.Pz():20.16f}        E : {item.E():20.16f}        P= {item.P():20.16f}"
+                    f"    Px : {item.Px():20.16f}   Py: {item .Py():20.16f}\
+                          Pz: {item.Pz():20.16f}    E : {item.E():20.16f}\
+                          P= {item.P():20.16f}"
                 )
     return
 
 
 def Broadcast(fatjets, check=False):
     """
-    Broadcast a list of fatjets to include an empty TLorentzVector for each fatjet.
+    Broadcast a list of fatjets to include an empty TLorentzVector for each
+    fatjet.
 
     Parameters:
     ----------
     fatjets : list of TLorentzVectors
         The input list of fatjets.
     check : bool, optional
-        Whether to check the broadcasting by comparing sums (default is False).
+        Whether to check the broadcasting by comparing sums
+        (default is False).
 
     Returns:
     -------
@@ -426,11 +459,12 @@ def Broadcast(fatjets, check=False):
 
     Notes:
     -----
-    This function takes a list of fatjets, and for each fatjet, it appends an empty TLorentzVector
-    to the end of the fatjet. It returns the resulting array of fatjets.
+    This function takes a list of fatjets, and for each fatjet,
+    it appends an empty TLorentzVector to the end of the fatjet.
+    It returns the resulting array of fatjets.
 
-    If 'check' is True, it also performs a check by comparing the sums of the original and
-    broadcasted fatjets.
+    If 'check' is True, it also performs a check by comparing the
+    sums of the original and broadcasted fatjets.
     """
     return_array = []
     for i in range(len(fatjets)):
@@ -444,7 +478,8 @@ def Broadcast(fatjets, check=False):
     return_array = np.array(return_array)
     if check:
         for i in range(len(fatjets)):
-            # print (fatjets[i].shape,return_array[i].shape,type(fatjets[i]),type(return_array[i]))
+            # print (fatjets[i].shape,return_array[i].shape,type(fatjets[i]),
+            # type(return_array[i]))
             s1, s2 = np.sum(fatjets[i]), np.sum(return_array[i])
             assert (
                 s1.M() == s2.M()
