c6cec0303ce4d8f0a9a057bad478d31182761c97
rajveer43 <rajveer.rathod1301@gmail.com>
2023-09-24 16:41:15 +0530

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -54,7 +54,7 @@ class Arrow3D(FancyArrowPatch):
         **kwargs: Additional keyword arguments.
 
     Attributes:
-        _verts3d (tuple):Tuple of (xs, ys, zs) representing the 3D coordinates
+        _verts3d (tuple): Tuple of (xs, ys, zs) representing the 3D coordinates
         of the arrow.
 
     Methods:
@@ -66,34 +66,15 @@ class Arrow3D(FancyArrowPatch):
     """
 
     def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
-        self._verts3d = (
-            xs,
-            ys,
-            zs,
-        )
+        super().__init__((0, 0), (0, 0), *args, **kwargs)
+        self._verts3d = (xs, ys, zs)
 
     def draw(self, renderer):
-        (
-            xs3d,
-            ys3d,
-            zs3d,
-        ) = self._verts3d
-        (
-            xs,
-            ys,
-            zs,
-        ) = proj3d.proj_transform(
-            xs3d,
-            ys3d,
-            zs3d,
-            renderer.M,
-        )
-        self.set_positions(
-            (xs[0], ys[0]),
-            (xs[1], ys[1]),
-        )
-        FancyArrowPatch.draw(self, renderer)
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        super().draw(renderer)
+
 
 
 class Plotter:
